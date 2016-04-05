3e1d20dc3cab67f335ee6d45770a0805267121b3
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-04-05 22:45:27 +0000

    Made a partial for article forms, added flashes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,29 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		@article.save
 
+		flash.notice = "Article '#{@article.title}' created!"
+
+		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' updated!"
+
 		redirect_to article_path(@article)
 	end
 
 	def destroy
-		Article.find(params[:id]).destroy
+		@article = Article.find(params[:id]).destroy
+
+		flash.notice = "Article '#{@article.title}' destroyed!"
+
 		redirect_to articles_path
 	end
 end
