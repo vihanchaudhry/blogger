70ed65489e698d134b5bbf19f261cca5008e718e
Vihan Chaudhry <vihanchaudhry@gmail.com>
2016-04-05 21:38:06 +0000

    Added delete button to article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,12 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.save
-		
+
 		redirect_to article_path(@article)
+	end
+
+	def destroy
+		Article.find(params[:id]).destroy
+		redirect_to articles_path
 	end
 end
